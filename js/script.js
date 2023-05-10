9bdc9d324e60dde704d9c2d534223a76747669d1
GabrielFaria-cyber <gabriel.faria56@etec.sp.gov.br>
2023-05-10 15:00:13 -0300

    .

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@ const CTX=canvas.getContext('2d');
 CTX.fillStyle='green';
 CTX.fillRect(10,10,150,100);
 
-
+CTX.beginPath();
+CTX.arc(400, 150, 150, 0, Math.PI*2);
+CTX.fillStyle='purple';
+CTX.fill();
