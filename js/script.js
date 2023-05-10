28024bb656368fcaaf540667cf07d1290c7480bc
GabrielFaria-cyber <gabriel.faria56@etec.sp.gov.br>
2023-05-10 15:43:25 -0300

    .

--- js/script.js
+++ js/script.js
@@ -9,11 +9,13 @@ CTX.fillStyle='purple';
 CTX.fill();
 
 CTX.beginPath();
-    CTX.arc(75, 300, 50, 0, Math.PI * 2, true); // Círculo exterior
-    CTX.moveTo(110, 300);
-    CTX.arc(75, 300, 35, 0, Math.PI, false);  // Boca (sentido horário)
-    CTX.moveTo(65, 300);
-    CTX.arc(60, 300, 5, 0, Math.PI * 2, true);  // Olho esquerdo
-    CTX.moveTo(95, 300);
-    CTX.arc(90, 300, 5, 0, Math.PI * 2, true);  // Olho direito
-    CTX.stroke();
+CTX.arc(75, 300, 50, 0, Math.PI * 2, true); // Círculo exterior
+CTX.moveTo(110, 300);
+CTX.arc(75, 300, 35, 0, Math.PI, false);  // Boca (sentido horário)
+CTX.moveTo(65, 290);
+CTX.arc(60, 290, 5, 0, Math.PI * 2, true);  // Olho esquerdo
+CTX.moveTo(95, 290);
+CTX.arc(90, 290, 5, 0, Math.PI * 2, true);  // Olho direito
+CTX.stroke();
+
+
