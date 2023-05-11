73ae98ae7fbe9cc612e9017ca2f28eb1fe876d0a
GabrielFaria-cyber <gabriel.faria56@etec.sp.gov.br>
2023-05-11 14:56:51 -0300

    Colocando //

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 const canvas=document.getElementById('canvas');
 const CTX=canvas.getContext('2d');
+
+//Retângulo
 CTX.fillStyle='green';
 CTX.fillRect(10,10,150,100);
 
+//Círculo
 CTX.beginPath();
 CTX.arc(400, 150, 150, 0, Math.PI*2);
 CTX.fillStyle='purple';
 CTX.fill();
 
+//Carinha feliz
 CTX.beginPath();
 CTX.arc(75, 300, 50, 0, Math.PI * 2, true); // Círculo exterior
 CTX.moveTo(110, 300);
@@ -17,5 +21,4 @@ CTX.arc(60, 290, 5, 0, Math.PI * 2, true);  // Olho esquerdo
 CTX.moveTo(95, 290);
 CTX.arc(90, 290, 5, 0, Math.PI * 2, true);  // Olho direito
 CTX.stroke();
-
 
